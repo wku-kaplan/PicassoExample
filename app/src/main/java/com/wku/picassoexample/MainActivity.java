c64d1fad68aca05f4fd210294aadadf0b55e29c5
William Ku <kucheech@gmail.com>
2017-10-11 17:16:46 +0800

    Initial commit

---
+++ app/src/main/java/com/wku/picassoexample/MainActivity.java
@@ -0,0 +1,51 @@
+package com.wku.picassoexample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText keyword = (EditText) findViewById(R.id.input);
+        final ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        final String baseurl = "http://loremflickr.com/320/240/";
+        final Random r = new Random();
+
+        final Picasso p = Picasso.with(getApplicationContext());
+
+        p.load(baseurl)
+                .placeholder(R.drawable.progress_animation)
+                .resize(320, 240)
+                .into(imageView);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            int i = 0;
+
+            @Override
+            public void onClick(View view) {
+                String url = baseurl + keyword.getText().toString() + "?random=" + i++;
+                p.load(url)
+                        .placeholder(R.drawable.progress_animation)
+                        .resize(320, 240)
+                        .centerCrop()
+                        .into(imageView);
+            }
+        });
+
+
+    }
+}
